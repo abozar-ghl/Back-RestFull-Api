6faa143495369b3281c2597ceac40329090a6fe9
abozar-ghl <abozar.245@gmail.com>
2021-03-14 14:32:27 +0330

    register 3

--- server.js
+++ server.js
@@ -10,6 +10,30 @@ const cors = require('cors')
 mongoose.connect('mongodb://127.0.0.1:27017/gashnix' , { useMongoClient : true });
 mongoose.Promise = global.Promise;
 
+
+//exist Sequance Collection 
+mongoose.connection.on('open', function (ref) {
+    console.log('Test Exist Colections....');
+    mongoose.connection.db.listCollections().toArray( (err, Collections) => {
+        // console.log(Collections); // [{ name: 'dbname.myCollection' }]  
+        // console.log(Collections)
+        // console.log(JSON.stringify(Collections))
+        console.log(Collections.findIndex(x => x.name === 'test'))
+        
+        for(i=0;i<Collections.length;i++)
+        {     
+            console.log(Collections[i].name);
+            
+        }
+
+        module.exports.Collection = Collections;
+        });
+})
+
+// const collections = Object.keys(mongoose.connection.collections);
+// console.log(JSON.stringify(collections))
+// console.log(mongoose.connection.names)
+
 //Cors
 
 
@@ -20,6 +44,8 @@ app.use('/public' , express.static('public'))
 
 const apiRouter = require('./modules/routes/api');
 const webRouter = require('./modules/routes/web');
+const { json } = require('body-parser');
+const { collection } = require('./modules/models/user');
 
 app.use('/api' , apiRouter)
 app.use('/' , webRouter);
